f3a0260a2cb359b58246279f285faf7ab09b0634
Ali AKGÖZ <nukenssd@gmail.com>
2025-11-26 09:24:00 +0300

    fix deployment errors: next config and missing tooltip

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['ccxt'],
-  },
+  serverExternalPackages: ['ccxt'],
 };
 
 export default nextConfig;
